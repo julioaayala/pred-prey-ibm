742cedbf33041f2f038d80a08d02492773d42aa4
Julio Ayala <jaayala@uvg.edu.gt>
2022-03-21 14:41:12 +0100

    Fix script template

--- Bash_scripts/pred_prey.sh
+++ Bash_scripts/pred_prey.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -A lu2021-2-124
 #SBATCH -N 4
-#SBATCH -t 4:00:00
+#SBATCH -t 10:00:00
 #SBATCH --mail-user=ju7141ay-s@student.lu.se
 #SBATCH --mail-type=FAIL
-#SBATCH -J pred_prey_alpha_pmutprey_gamma_aP_efficiency_pmutpred_morphsinit
-#SBATCH -o Logs/pred_prey_alpha_pmutprey_gamma_aP_efficiency_pmutpred_morphsinit.out
-#SBATCH -e Logs/pred_prey_alpha_pmutprey_gamma_aP_efficiency_pmutpred_morphsinit.err
+#SBATCH -J pred_prey_alpha_pmutprey_gamma_aP_efficiency_pmutpred_morphsinit_c_a_prey_c_a_pred_is_sexual
+#SBATCH -o Logs/pred_prey_alpha_pmutprey_gamma_aP_efficiency_pmutpred_morphsinit_c_a_prey_c_a_pred_is_sexual.out
+#SBATCH -e Logs/pred_prey_alpha_pmutprey_gamma_aP_efficiency_pmutpred_morphsinit_c_a_prey_c_a_pred_is_sexual.err
 #SBATCH -D /home/ayalaj/Pred_prey_dynamics/pred-prey-ibm
 
 module load matlab
